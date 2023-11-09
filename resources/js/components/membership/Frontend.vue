5670fa53b6e2ab606bb34ff1d1aa4e38f0675d81
arhamnatiq25@gmail.com <arhamnatiq25@gmail.com>
2023-11-09 15:22:23 +0500

    add comments

--- resources/js/components/membership/Frontend.vue
+++ resources/js/components/membership/Frontend.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container mb-4">
+    <!-- when member is saved or customer payment successfull it shows for 10 sec  -->
     <div v-if="successMessage" class="alert alert-success" role="alert">
       {{ successMessage }}
     </div>
+    <!-- when get errors from backend during save of member shows modified arrays of errors here  -->
     <div v-for="(error, index) in Object.keys(errors)" :key="index">
     <div v-if="errors[error].length > 0">
       <div class="alert alert-danger" role="alert" v-for="(message, i) in errors[error]" :key="i">
@@ -21,6 +23,7 @@
             <div class="card-container">
               <h4>Membership Details</h4>
               <div class="row mt-3">
+                <!-- Show membershiips that are active although only one membership is actived at a time  -->
                 <div class="col-md-6" v-for="item in list" :key="item.id">
                   <div class="card" style="margin-bottom: 20px">
                     <div class="card-body">
@@ -62,6 +65,7 @@
               <label for="membershipBuying"
                 >How many Memberships do you want to buy ?
               </label>
+              <!-- Here how many we input it gets in membershipCount  -->
               <input
                 type="number"
                 class="form-control"
@@ -72,6 +76,7 @@
             </div>
           </div>
           <div class="col-md-2 mt-md-4">
+            <!-- on click of this button function of show User Detaisl is called  -->
             <button class="btn btn-warning mt-1" @click="showUserDetails">
               Next
             </button>
@@ -189,6 +194,7 @@
     </div>
     <!-- Customers Area  -->
     <div v-show="showCheckout">
+        <!-- here errors shown that occurs during payment  -->
         <div v-if="errorMessage">
       <p class="alert alert-danger">{{ errorMessage }}</p>
           </div>
@@ -253,6 +259,7 @@
 <script>
 import axios from "axios";
 export default {
+    // name use to export in app.js..
   name: "Frontend",
   // Data that are used in table to manuplate ....
   data() {
@@ -289,9 +296,13 @@ export default {
   mounted() {
     // here calling method to fetech all data
     this.fetchAll();
+    // this is use to call stripe every time when our page is ready or refresh
     this.loadStripe();
   },
+
   computed: {
+
+    // here is the function that comutes total payment ..
     totalPayment() {
       const membershipPrice = this.items.membership_price;
       const numberOfMembers = this.items.memberships.length;
@@ -308,12 +319,15 @@ export default {
         this.items.membership_price=this.list[0].first_price
       });
     },
+    // this function calculate total payment ..
     calculateTotalPayment() {
     const membershipPrice = this.items.membership_price;
     const numberOfMembers = this.items.memberships.length;
     return membershipPrice * numberOfMembers;
   },
+  // This functions shows how many members we want to add . it shows forms like 1 , 2 , 3
     showUserDetails() {
+        // we put v model in input of membership count and here loop to display N forms n=1,2,3,4...
       for (let i = 0; i < parseInt(this.membershipCount); i++) {
         this.items.memberships.push({
           fname: "",
@@ -321,12 +335,13 @@ export default {
           dob: "",
           gender: "",
           activity: "",
-          // Membership ID can be the same for all members
         });
       }
+      // make user details true and membership page hide ..
       this.showUserDetailsForm = true;
       this.showMemberships = false;
     },
+    // this function is called when click on back button so it goes to show membership and clear all fields
     goBack() {
       this.showMemberships = true;
       this.showUserDetailsForm = false;
@@ -336,6 +351,7 @@ export default {
       this.resetFormFields();
 
     },
+    // this function is called and we push data in our membershiip array everytime doesnot matter how many records we have
     addMember() {
       this.items.memberships.push({
         fname: this.items.fname,
@@ -344,7 +360,7 @@ export default {
         gender: this.items.gender,
         activity: this.items.activity,
       });
-
+      // After pushing data in array ....
       // Clear the form fields for the next member
       this.items.fname = "";
       this.items.lname = "";
@@ -352,12 +368,18 @@ export default {
       this.items.gender = "";
       this.items.activity = "";
     },
+
+    // whenever this fun is called all errors clear ...
     clearErrors() {
       this.errors = {}; // Clear errors
     },
+    // when our page loads this function called as we called it from mount...
     loadStripe() {
+        // when window refresuh
       if (window.Stripe) {
+        // assign stripe a publish key of our stripe
         this.stripe = window.Stripe('pk_test_51NUU03Emu0Ala7lxKFLz0kgK8mfOVQr99wlJMIDW39xzneQ0B6Zb2x9irWjjNuldkUYyDFQG11FE50M6px3wvrVx00A0milkpo');
+        // assign elements .  a stripe elements of card , cvc , etc
         this.elements = this.stripe.elements();
 
         // Create an instance of the card Element
@@ -370,6 +392,7 @@ export default {
         this.errorMessage = 'Stripe is not available';
       }
     },
+    // whenever we called this fun all fields reset except membership id and membership price
     resetFormFields() {
     this.items = {
       memberships: [],
@@ -389,7 +412,9 @@ export default {
     this.membershipCount = 0;
     this.errors = {};
   },
- async submitPayment() {
+  // this function is called when we click on PAYMENT BUTTON.
+    async submitPayment() {
+      // here create payment method / error and get card elements . if any error it shows error
       const { paymentMethod, error } = await this.stripe.createPaymentMethod({
         type: 'card',
         card: this.elements.getElement('card'),
@@ -398,13 +423,17 @@ export default {
       if (error) {
         this.errorMessage = error.message;
       } else {
+        //here call a next function with payment id we
         // Send paymentMethod.id to the backend to process the payment
         this.processPayment(paymentMethod.id);
       }
     },
+    // this function is called from submit Function and it recieves payment id which we send to laravel controller
     async processPayment(paymentMethodId) {
       try {
+        // here total payment get by calling the function
         const totalPayment = this.calculateTotalPayment(); // Calculate the total payment
+        // request using api and send body ...
         const response = await fetch('/api/process-payment', {
           method: 'POST',
           headers: {
@@ -422,9 +451,10 @@ export default {
         });
 
 
-
+        // get response from backend ..
         const responseData = await response.json();
         // Handle the response from the server after payment processing
+        //if success show success messgae for 10 sec
       if (responseData.success) {
         this.successMessage = responseData.message;
 
@@ -438,6 +468,7 @@ export default {
           this.successMessage = "";
         }, 10000);
 
+        // at the end reset all fields and show memberhip page ....
         this.resetFormFields();
       }
       } catch (error) {
@@ -447,8 +478,12 @@ export default {
 
     // members save in database :
     memberSave() {
+        // when click on next button this fun called .
+        // firstly it clear errors
         this.clearErrors();
       try {
+        // request through api to backend and send mebership array consist of memberss ..
+        // id of that membership that is active
         axios
           .post("/api/member", {
             memberships: this.items.memberships,
@@ -463,6 +498,7 @@ export default {
               gender: "",
               activity: "",
             }));
+            // get a response of member ids that are saved in database and get them in savedMemberIdDs array
             this.savedMemberIDs = response.data.savedMemberIDs;
             // Display the success message
             this.successMessage = response.data.message;
@@ -483,6 +519,7 @@ export default {
           // Capture errors from the backend and assign them to the errors object
           const backendErrors = error.response.data.error;
 
+          // use Nested loops to customize errors that are coming from backend
           for (const key in backendErrors) {
             const index = key.match(/\d+/); // Extract the index from the key
             const fieldName = key.replace(/\.\d+\./, " "); // Extract field name
